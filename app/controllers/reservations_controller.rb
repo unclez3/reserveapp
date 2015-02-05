0d2b06b8210c1ee69c66865d08f7a539c31c58f1
marukiya <yu.tsukada@gmail.com>
2015-02-05 02:42:25 +0000

    newpage added

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,13 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+  end
+   
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+  
+  def new
+    @reservation = Reservation.new
+  end
 end
